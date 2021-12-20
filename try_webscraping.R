d76d024e7bac700ac4b23837412a29b1a1798e0e
lauramenicacci <laura@menicacci.eu>
2021-12-20 18:47:32 +0100

    polite webscraping done

--- try_webscraping.R
+++ try_webscraping.R
@@ -1,4 +1,4 @@
-##polite coding 
+##polite webscraping
 
 # criteria for polite code:
 # only download if file does not exist yet
@@ -10,21 +10,9 @@ pageurl <- paste0(seq(1, 33, 1))
 pages_list <- paste0(baseurl, pageurl)
 pages_list[1:5] #33 pages until 18 oct = 2 weeks before cop 
 headlineslist <- c()
-dateslist <- c()
 
-#headings_filenames <- pages_list
 folder <- "C:/Users/laura/Desktop/guardian_htmls/"
 dir.create(folder)
-for (i in 1:length(pages_list)) {headlineslist[i] <- paste0("page", i)} {
-  if (!file.exists(paste0(folder, headlineslist[i], ".html"))) {
-    try(download.file(pages_list[i], 
-                      destfile = paste0(folder, headlineslist[i], ".html"),
-                      header = c(`User-Agent` = R.Version()$version.string, `From` = "212166@hertie-school.org")))
-    Sys.sleep(runif(1, 1, 2))
-  }
-}
-
-
 for (i in 1:length(pages_list)) {headlineslist[i] <- paste0("page", i)}
 
 for (i in 1:length(pages_list))  { if (!file.exists(paste0(folder, headlineslist[i]))) {  
@@ -38,12 +26,6 @@ list.files(folder, pattern = ".*",full.names = TRUE)
 
 
 #############
-baseurl <- "https://www.theguardian.com/environment/cop26-glasgow-climate-change-conference-2021?page="
-pageurl <- paste0(seq(1, 33, 1))
-pages_list <- paste0(baseurl, pageurl)
-pages_list[1:5] #33 pages until 18 oct = 2 weeks before cop 
-headlineslist <- c()
-dateslist <- c()
 
 for (page in pages_list){
   url_parsed <- read_html(page)
